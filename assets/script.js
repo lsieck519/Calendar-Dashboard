a7e763511ea2831fbdf0ed3e7d557da63b8820c8
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-01-11 06:54:37 -0500

    added bulma modal

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,8 @@ function trendingMoives(){
             for (var i = 0; i < data.length; i ++){
               let movieData = {}
               movieData.title = data[i].original_title
-              movieData.poster = "https://image.tmdb.org/t/p/w500" + data[i].poster_path 
+              movieData.poster = "https://image.tmdb.org/t/p/w300" + data[i].poster_path 
+              movieData.overview = data[i].overview
               allMovies.push(movieData)
             }
             console.log(allMovies)
@@ -140,3 +141,49 @@ function getSchedule() {
 }
 
 getSchedule();
+
+//modal code for bulma
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
